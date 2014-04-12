07c92adf8780ac3310d37cb505f3e62a195cb0f4
fluzzi <fluzzi@gmail.com>
2014-04-12 15:41:11 -0300

    Fit video to screen plugin.

--- app/js/player.js
+++ app/js/player.js
@@ -91,7 +91,7 @@ var Player = function() {
 
 	    video.mediaelementplayer({
 			videoVolume: 'horizontal',
-			features: ['playpause','current','progress','duration','fullscreen','volume','tracks','torrentinfo','fontawesome', 'customtracks'],
+			features: ['playpause','current','progress','duration','fullscreen','volume','tracks','videofit','torrentinfo','fontawesome', 'customtracks'],
 			success : function(mediaElement, domObject, player) {
         		t.mePlayer = player;
         		// TODO: Move me into a mediaelement plugin?
